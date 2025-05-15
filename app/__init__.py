aab60b2c45c46b271da9fad8d78ead59144d3361
duskBs <119872396+BlakeIvor@users.noreply.github.com>
2025-05-14 23:58:34 -0400

    Add following and followers

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from config import Config
 import os
+import logging
+from logging.handlers import RotatingFileHandler
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -20,4 +22,18 @@ manager.login_view = 'login'
 from app.models import load_user
 manager.user_loader(load_user)
 
-from app import routes, models
+from app import routes, models, errors
+
+if not app.debug:
+    # ...
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
